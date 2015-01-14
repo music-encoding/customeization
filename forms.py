198240a4d843d4a1d8c43b1d5adcee1f4dc23808
Andrew Hankinson <andrew.hankinson@gmail.com>
2015-01-13 19:31:05 -0500

    Adjust menu names

--- forms.py
+++ forms.py
@@ -13,9 +13,9 @@ class ProcessForm(Form):
     CUSTOMIZATION_OPTIONS = [(k, conf.AVAILABLE_CUSTOMIZATIONS[k][0]) for k in sorted(conf.AVAILABLE_CUSTOMIZATIONS)]
 
     schema_language = SelectField(u'Schema Language',
-                                 choices=[('relaxng', 'RelaxNG'),
+                                 choices=[('relaxng', 'RelaxNG Schema'),
                                           ('compiledodd', 'Compiled ODD'),
-                                          ('documentation', 'Documentation')],
+                                          ('documentation', 'HTML Documentation')],
                                  validators=[InputRequired()])
 
     source_options = SelectField(u'MEI Source',
